3ea4ef300992f4a0fbe2d40c33aea05d8b92d28e
hturcin <h.turcin@hotmail.com>
2021-03-22 13:54:30 +0100

    My changes before chaper 10, Updating, showing, and deleting users

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -23,6 +23,7 @@ class ActiveSupport::TestCase
 
   # Add more helper methods to be used by all tests here...
   include ApplicationHelper
+  include SessionsHelper
 end
 
 class ActionDispatch::IntegrationTest
