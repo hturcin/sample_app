50128e0a424039fbb2e4c6342ceaeb6ec1473658
hturcin <h.turcin@hotmail.com>
2021-03-24 18:11:14 +0100

    Stopped at 10.3 in the book

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -29,6 +29,11 @@ module SessionsHelper
     end
   end
 
+  # Returns true if the given user is the current user.
+  def current_user?(user)
+    user && user == current_user
+  end
+
   # Returns true if the user is logged in, false otherwise.
   def logged_in?
     !current_user.nil?
@@ -46,6 +51,17 @@ module SessionsHelper
     forget(current_user)
     session.delete(:user_id) # deleting user from session
     @current_user = nil # security measure
+  end
+
+  # Redirects to stored location (or to the default).
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # Stores the URL trying to be accessed.
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
   end
 
 end
