3348b0e750bb9d70d8be69a0d850706dfce75203
hturcin <h.turcin@hotmail.com>
2021-03-29 14:00:04 +0200

    Add account activation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
     :user_name        => ENV['SENDGRID_USERNAME'],
     :password         => ENV['SENDGRIG_PASSWORD'],
     :domain           => 'heroku.com',
-    :enable_starttls_auto => truehero
+    :enable_starttls_auto => true
   }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
