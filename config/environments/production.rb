3374860e2123f8a33adb44b9e57c832974da5897
hturcin <h.turcin@hotmail.com>
2021-03-29 17:28:08 +0200

    Changed mailing settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,10 +70,10 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   host = 'www.sample-app-hrv.herokuapp.com'
   config.action_mailer.default_url_options = { host: host}
-  ActionMailer::Base.smtp_settings = {
+  config.action_mailer.smtp_settings = {
     :address          => 'smtp.gmail.com',
     :port             => 587,
-    :authentication   => :login,
+    :authentication   => 'plain',
     :user_name        => ENV['GMAIL_USERNAME'],
     :password         => ENV['GMAIL_PASSWORD'],
     :domain           => 'gmail.com',
