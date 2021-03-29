86d73e111e30901424648ea0d89a9ed0f3022fc4
hturcin <h.turcin@hotmail.com>
2021-03-29 21:57:26 +0200

    Some changes to mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'www.sample-app-hrv.herokuapp.com'
+  host = 'sample-app-hrv.herokuapp.com'
   config.action_mailer.default_url_options = { host: host}
   config.action_mailer.smtp_settings = {
     :address          => 'smtp.gmail.com',
